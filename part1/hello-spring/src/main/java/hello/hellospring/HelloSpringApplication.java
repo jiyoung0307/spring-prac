8d374c38f50a40313f9996001ed2ea3b56d6b51c
zy_zero <jiyoungjang0307@gmail.com>
2025-05-11 22:35:20 +0900

    setting complete

--- part1/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ part1/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -8,6 +8,7 @@ public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
+		System.out.println("part1.스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술");
 	}
 
 }

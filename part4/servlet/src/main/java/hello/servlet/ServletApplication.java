7ebfd5d1d04fafca5398531ac53ea14247640d61
zy_zero <jiyoungjang0307@gmail.com>
2026-05-04 19:33:25 +0900

    Sec3.Servelt - Hello Servlet

--- part4/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ part4/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,10 +2,11 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan	/* 서블릿 자동 등록 */
 @SpringBootApplication
 public class ServletApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 		System.out.println("스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술");
